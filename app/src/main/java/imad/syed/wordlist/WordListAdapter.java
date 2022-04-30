923152d49afc8367ee6cc137a34d891111b21889
imadsyed33 <creeperone23@gmail.com>
2022-04-30 01:17:51 -0400

    API data fetch added to onClick method of recyclerView items

--- app/src/main/java/imad/syed/wordlist/WordListAdapter.java
+++ app/src/main/java/imad/syed/wordlist/WordListAdapter.java
@@ -1,5 +1,6 @@
 package imad.syed.wordlist;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,6 +8,17 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,10 +44,38 @@ public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordLi
         }
     }
 
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            TextView nameItem = view.findViewById(R.id.nameView);
+            TextView meaningItem = view.findViewById(R.id.meaningView);
+            RequestQueue requestQueue = Volley.newRequestQueue(view.getContext());
+            String url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + nameItem.getText();
+            JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+                @Override
+                public void onResponse(JSONArray response) {
+                    try {
+                        String meaning = response.getJSONObject(0).getJSONArray("meanings").getJSONObject(0).getJSONArray("definitions").getJSONObject(0).get("definition").toString();
+                        meaningItem.setText(meaning);
+                    } catch (JSONException e) {
+                        Log.d("error ", "badness");
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d("error ", "request failed");
+                }
+            });
+            requestQueue.add(jsonArrayRequest);
+        }
+    };
+
     @NonNull
     @Override
     public WordListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row, parent, false);
+        view.setOnClickListener(onClickListener);
         return new WordListViewHolder(view);
     }
 
